70776f2fc7001de9ca7f2eeb4179d676eef80b47
kenny <kennyooi@inwebson.com>
2023-01-20 17:45:42 +0800

    Extraction How it works copywriting.

--- components/Hiw/SectionLifestyle.vue
+++ components/Hiw/SectionLifestyle.vue
@@ -2,14 +2,15 @@
   <div id="Lifestyles" class="section-lifestyles">
     <div class="container">
       <h5 class="font-label mb-3 text-lg-center">
-        YOUR JOURNEY WITH DETALYTICS
+        {{ $t('HIW.LIFESTYLE.LABEL') }}
       </h5>
       <h2 class="font-title text-lg-center">
-        Evidence-based life and work transformation in every aspect of your lifestyle
+        {{ $t('HIW.LIFESTYLE.TITLE') }}
       </h2>
-      <div class="font-desc text-lg-center">
-        Gain key personalised  insights into your body, mind and health with baseline assessments. From sleep, activities to diet, optimise your lifestyle behaviours and impact your goals.
-      </div>
+      <div
+        v-html="$t('HIW.LIFESTYLE.DESC')"
+        class="font-desc text-lg-center"
+      ></div>
       <div class="img text-center">
         <img src="/images/hiw/lifestyles-chart.png" alt="Detalytics lifestyles" width="1205" height="938" loading="lazy">
       </div>
